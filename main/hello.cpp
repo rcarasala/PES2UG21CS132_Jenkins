77a51262d032a80ef6badffac5b3151ef486978a
rcarasala <149596369+rcarasala@users.noreply.github.com>
2024-03-12 19:33:40 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run PES2UG21CS132\n";
+  cout << "I have successfully built and run PES2UG21CS132 changes made\n";
   return 0;
 }
